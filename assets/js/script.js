a200513f8c81cf5a2d4e3630ae2cb5d8caa56214
Ted Pedersen <tedpedersen@me.com>
2020-06-29 19:14:45 -0700

    externalize google map js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// clicking intro buttons to scroll to stats and map
+function scrollToStats(div){
+    $('html, body').animate({
+        scrollTop: $("#stats").offset().top
+},500);
+}
+function scrollToMap(div){
+    $('html, body').animate({
+        scrollTop: $("#pac-card").offset().top
+},500);
+}
