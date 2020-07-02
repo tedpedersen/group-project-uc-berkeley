59d669b36773316b4cab430ccfa854eea9d8a4d1
Vanessa Lane <vlane@23andme.com>
2020-07-02 15:46:34 -0700

    surface worldwide cases on load

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,3 +1,31 @@
-// update current date
+
+
+// api calls
+var getGlobalStats = function() {
+    /**
+     * Gets the global summary stats to surface on load
+     **/
+    var apiUrl = "https://api.covid19api.com/summary";
+    fetch(apiUrl).then(function(res) {
+        if (res.ok) {
+            res.json().then(function(data) {
+                // converts the Global Data object into an array
+                var globalData = Object.entries(data['Global']);
+                for (let i=0; i<globalData.length; i++) {
+                    var stat = toString(globalData[i][1]);
+                    $("#" + globalData[i][0]).text(globalData[i][1]);
+                }
+            })
+        } else {
+            console.log("something went wrong");
+        }
+    })
+}
+
+// update current date on load
 var currentDate = moment().format("dddd, MMMM Do, YYYY");
 $("#current-date").text(currentDate);
+
+// surface US stats on load
+getGlobalStats();
+
