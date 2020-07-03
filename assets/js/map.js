f78157691f86610aa73d69f193f3f73413133cef
Ted Pedersen <tedpedersen@me.com>
2020-07-03 11:59:26 -0700

    remove config file

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,4 +1,3 @@
-var googlekey = config.googlekey;
 //no data available for South Dakota or Rhode Island
 var statesArray = ['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado', 'connecticut', 'delaware', 'florida', 'georgia', 'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'mississippi', 'missouri', 'montana', 'nebraska', 'nevada', 'new-hampshire', 'new-jersey', 'new-mexico', 'new-york', 'north-carolina', 'north-dakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', 'south-carolina', 'tennessee', 'texas', 'utah', 'vermont', 'virginia', 'washington', 'west-virginia', 'wisconsin', 'wyoming']
 
