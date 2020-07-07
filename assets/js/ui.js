bf39c4d38695fc34b47e38f899dc357b5d14c88a
Ted Pedersen <tedpedersen@me.com>
2020-07-07 13:58:10 -0700

    dynamic active link style

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -3,4 +3,14 @@ function scrollToStats(div){
     $('html, body').animate({
         scrollTop: $("#stats-container").offset().top + -160
 },500);
+//dynamically add active style to nav links
+$(".uk-navbar-container ul li").click(function(){   
+    $(this).addClass('uk-active');
+    $(this).siblings().removeClass('uk-active');
+   });
 }
+var url = window.location.href;
+var statNav = document.getElementById("statNav");
+if (url.search("#stats") >= 0) {
+    statNav.classList.add("uk-active");
+} 
